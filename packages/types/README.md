7037be743b0eade9426d83e61447ed941311f77e
Anton Golub <antongolub@antongolub.com>
2023-06-11 14:14:43 +0300

    feat: introduce types package

---
+++ packages/types/README.md
@@ -0,0 +1,16 @@
+# @abstractest/types
+> TS types for [abstractest](https://github.com/antongolub/abstractest)
+
+## Usage
+```ts
+import '@abstractest/types'
+
+describe('foo', () => {
+  it('bar', () => {
+    
+  })
+})
+```
+
+## License
+[MIT](./LICENSE)
---
+++ packages/types/src/dts/index.d.ts
@@ -0,0 +1,61 @@
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/node/test.d.ts
+
+declare module '@abstractest/types' {
+  export type Done = (result?: any) => void
+
+  export type TestFn = (done: Done) => void | Promise<void>
+
+  export type Test = (name: string, fn?: TestFn) => void
+
+  export type It = Test & {
+    only: Test
+    skip: Test
+    todo: Test
+  }
+
+  export type SuiteFn = (done: Done) => void
+
+  export type DescribeSuite = (name: string, fn?: SuiteFn) => void
+
+  export type Describe = DescribeSuite & {
+    only: DescribeSuite
+    skip: DescribeSuite
+    todo: DescribeSuite
+  }
+
+  export type HookFn = (done: Done) => any
+
+  export type Hook = (fn: HookFn) => void | Promise<void>
+
+  export type TestApi = {
+    describe: Describe
+    it: It,
+    expect: Expect
+    before: Hook
+    beforeEach: Hook
+    after: Hook
+    afterEach: Hook
+  }
+
+  export type Run = (opts: {cwd: string, include: string[]}) => Promise<any>
+
+  export type Runner = {
+    name: string
+    run: Run
+    api: TestApi
+  }
+
+  export type Expect = (val: any) => {
+    toEqual(val: any): void
+  }
+
+  global {
+    const it: It
+    const describe: Describe
+    const expect: Expect
+    const before: Hook
+    const beforeEach: Hook
+    const after: Hook
+    const afterEach: Hook
+  }
+}
